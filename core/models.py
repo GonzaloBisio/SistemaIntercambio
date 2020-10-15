5dce3b7d5d246bad39ef6be9e9aacf07fe00c1a0
GonzaloBisio2002.Gonzalo <GonzaloBisio2002.crackbisio@gmail.com>
2020-10-15 12:26:18 -0300

    seguimos con los models

--- core/models.py
+++ core/models.py
@@ -3,11 +3,19 @@ from django import forms
 
 # Create your models here.
 
+ESTADO = [
+    ('NR', 'No registrado'),
+    ('R', 'Registrado'),
+    ('ET', 'En trámite'),
+    ('EV', 'Esperando vuelo'),
+]
 
 class RegisterTraveller(models.Model):
     codigo = models.CharField(max_length=50)
+    status = models.CharField(max_length=2, choices=ESTADO, default= 'No registrado')
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
+    edad = models.IntegerField(default=0)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     pais = models.CharField(max_length=50)
@@ -18,3 +26,14 @@ class UserForm(forms.ModelForm):
         model = RegisterTraveller
         fields = ['password']
         widgets = {'password': forms.PasswordInput(),}
+
+class RegisterHost(models.Model):
+    codigo = models.CharField(max_length=50)
+    status = models.CharField(max_length=2, choices=ESTADO, default= 'No registrado')
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.DateField()
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    pais = models.CharField(max_length=50)
+    estado = models.CharField(max_length =50)
