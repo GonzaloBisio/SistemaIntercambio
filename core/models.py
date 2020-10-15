3de1345ce6f2c022bab6b6cca790b58a0cc0c402
GonzaloBisio2002.Gonzalo <GonzaloBisio2002.crackbisio@gmail.com>
2020-10-15 11:31:35 -0300

    Comienzo con los models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django import forms    
 
 # Create your models here.
+
+
+class RegisterTraveller(models.Model):
+    codigo = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    pais = models.CharField(max_length=50)
+    estado = models.CharField(max_length =50)
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = RegisterTraveller
+        fields = ['password']
+        widgets = {'password': forms.PasswordInput(),}
