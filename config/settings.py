02aa6588869581a98e623194a1af6cf3ea7ad141
GonzaloBisio2002.Gonzalo <GonzaloBisio2002.crackbisio@gmail.com>
2020-12-02 12:13:39 -0300

    Actualizaciones

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,4 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = 'index'
+
--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -15,3 +17,7 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
